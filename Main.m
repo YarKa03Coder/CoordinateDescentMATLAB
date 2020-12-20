e40d0ac621009fad6839771409c00f3dd1313122
YarKa03Coder <yaroslav0302001@gmail.com>
2020-12-20 13:04:48 +0200

    Add comments to seperate 2D and 3D plots

--- Main.m
+++ Main.m
@@ -9,11 +9,13 @@ disp(instance); % Properties of instance before invocation method
 disp('Properties of instance after invocation of the method');
 disp(instance); % new values of the properties of the instance after optimum was found
 
+% 2-D plot
 figure;
 plot(X,Y,'b*-'), 
 title({['2-D plot'],['h = ',num2str(instance.h),blanks(3),'i = ',num2str(instance.iterationCount)]}),
 axis equal, grid on;
 
+% 3-D plots
 figure;
 [meshX, meshY] = meshgrid(X,Y);
 mesh(meshX, meshY, instance.equation(meshX, meshY)),
